7cb748ba73e1ddc36d3c5996af7d3417e31be47b
Vivek Patel <vivek.p9737@gmail.com>
2021-04-05 16:17:02 +0530

    init react with materialize css

---
+++ one-click-ai/src/App.js
@@ -0,0 +1,10 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import Home from './views/Home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ one-click-ai/src/views/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div className="container text-center">Hello World!</div>
+        )
+    }
+}
+
+export default Home
